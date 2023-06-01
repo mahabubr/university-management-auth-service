ab749234ba0781376cbfa42f5bf0c3c16cc004f6
mahabubr <mahabub.r753@gmail.com>
2023-06-02 01:44:36 +0600

    es lint pretter

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
 //CORS
-app.use(cors());
+app.use(cors())
 
 // PARSER
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Testing
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: any) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
