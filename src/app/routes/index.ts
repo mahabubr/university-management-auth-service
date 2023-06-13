84d2ecd5bb0b27cd5bfd707bc292eeebf731b645
mahabubr <mahabub.r753@gmail.com>
2023-06-13 13:28:33 +0600

    Academic Faculty [DONE]

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { UserRoutes } from '../modules/user/user.route';
 import { AcademicSemesterRoute } from '../modules/academicSemester/academicSemester.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: '/academic-semesters/',
     route: AcademicSemesterRoute,
+  },
+  {
+    path: '/academic-faculties/',
+    route: AcademicFacultyRoutes,
   },
 ];
 
